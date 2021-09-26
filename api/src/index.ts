b5e5394d44019875c4474e0a6947874508806b06
Jonathan Alvarez <j@jonalvarezz.com>
2021-09-26 06:30:10 +0200

    Spin up Apollo server
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

---
+++ api/src/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server'
+
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+  },
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
