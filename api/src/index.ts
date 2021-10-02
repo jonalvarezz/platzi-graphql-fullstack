5de9dd41b7963edd566708428c99458d81fab31a
Jonathan Alvarez <j@jonalvarezz.com>
2021-10-02 19:34:21 +0200

    Resolvers: Wire Prisma up
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,18 @@
 import { ApolloServer } from 'apollo-server'
 import path from 'path'
+import { PrismaClient } from '@prisma/client'
 import { readFileSync } from 'fs'
 import resolvers from './resolvers'
 
 const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8')
+const orm = new PrismaClient()
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    orm,
+  },
 })
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
--- api/src/modules/avocado/avocado.resolver.ts
+++ api/src/modules/avocado/avocado.resolver.ts
@@ -1,38 +1,41 @@
 import { createHash } from 'crypto'
-import { baseModelResolver } from '../base/base.resolver'
-import { Avocado } from './avocado.model'
+import type { PrismaClient, Avocado, Attributes } from '@prisma/client'
 
-const avos: Avocado[] = [
-  {
-    createdAt: new Date(),
-    updatedAt: undefined,
-    deletedAt: undefined,
-    name: 'Pinkerton Avocado',
-    id: 'fpr72m9k',
-    sku: 'B4HZ42TM',
-    price: 1.27,
-    image: '/images/pinkerton.jpg',
-    attributes: {
-      description:
-        "First grown on the Pinkerton Ranch in Saticoy, California, in the early 1970s, 'Pinkerton' is a seedling of 'Hass' x 'Rincon'. The large fruit has a small seed, and its green skin deepens in color as it ripens. The thick flesh has a smooth, creamy texture, pale green color, good flavor, and high oil content. It shows some cold tolerance, to −1 °C (30 °F) and bears consistently heavy crops. A hybrid Guatemalan type, it has excellent peeling characteristics",
-      shape: 'Long pear',
-      hardiness: '−1 °C',
-      taste: 'Marvelous, is an avocado',
+type ResolverParent = unknown
+type ResolverContext = { orm: PrismaClient }
+
+export async function findAll(
+  parent: ResolverParent,
+  args: undefined,
+  context: ResolverContext
+): Promise<Avocado[]> {
+  return context.orm.avocado.findMany({ include: { attributes: true } })
+}
+
+export async function findOne(
+  parent: ResolverParent,
+  args: { id: number },
+  context: ResolverContext
+): Promise<Avocado | null> {
+  return context.orm.avocado.findUnique({
+    where: args,
+    include: {
+      attributes: true,
     },
-  },
-]
-
-export function findAll(): Avocado[] {
-  return avos
+  })
 }
 
-export function findOne(id: string): Avocado | null {
-  return avos[0]
-}
+type AvocadoInput = Pick<Avocado, 'name' | 'price' | 'image'> &
+  Omit<Attributes, 'id'>
 
-export const resolver: Record<keyof Avocado, (parent: Avocado) => unknown> = {
-  ...baseModelResolver,
+export const resolver: Record<
+  keyof (Avocado & { attributes: Attributes }),
+  (parent: Avocado & { attributes: Attributes }) => unknown
+> = {
   id: (parent) => parent.id,
+  createdAt: (parent) => parent.createdAt,
+  deletedAt: (parent) => parent.deletedAt,
+  updatedAt: (parent) => parent.updatedAt,
   sku: (parent) => parent.sku,
   name: (parent) => parent.name,
   price: (parent) => parent.price,
@@ -45,33 +48,33 @@ export const resolver: Record<keyof Avocado, (parent: Avocado) => unknown> = {
   }),
 }
 
-export function createAvo(
+export async function createAvo(
   parent: unknown,
   {
     data,
-  }: { data: Pick<Avocado, 'name' | 'price' | 'image'> & Avocado['attributes'] }
-): Avocado {
-  const currentLength = avos.length
-  const newAvo: Avocado = {
-    id: String(currentLength + 1),
-    sku: createHash('sha256')
-      .update(data.name, 'utf8')
-      .digest('base64')
-      .slice(-6),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    deletedAt: undefined,
-    name: data.name,
-    price: data.price,
-    image: data.image,
-    attributes: {
-      description: data.description,
-      shape: data.shape,
-      hardiness: data.hardiness,
-      taste: data.taste,
-    },
-  }
+  }: {
+    data: AvocadoInput
+  },
+  { orm }: { orm: PrismaClient }
+): Promise<Avocado> {
+  const { name, image, price, ...attributes } = data
 
-  avos.push(newAvo)
-  return newAvo
+  const sku = createHash('sha256')
+    .update(`${name}-${price}`, 'utf8')
+    .digest('base64')
+    .slice(-6)
+
+  const avo = await orm.avocado.create({
+    data: {
+      name,
+      price,
+      image,
+      sku,
+      attributes: {
+        create: attributes,
+      },
+    },
+  })
+
+  return avo
 }
