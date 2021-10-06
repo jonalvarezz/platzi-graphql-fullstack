e24915a2084a050cf8f30aee837aa7cecb6f14dd
Jonathan Alvarez <j@jonalvarezz.com>
2021-10-07 00:51:22 +0200

    Use Express and configure static folder
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,7 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
 import path from 'path'
 import { PrismaClient } from '@prisma/client'
 import { readFileSync } from 'fs'
@@ -7,12 +10,37 @@ import resolvers from './resolvers'
 const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8')
 const orm = new PrismaClient()
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: {
-    orm,
-  },
-})
+!(async function () {
+  // Required logic for integrating with Express
+  const app = express()
+  const httpServer = http.createServer(app)
+  // Middlewares
+  app.use('/static', express.static(path.join(__dirname, '../public')))
 
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
+  // Same ApolloServer initialization as before, plus the drain plugin.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+      orm,
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  // More required logic for integrating with Express
+  await server.start()
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/graphql',
+  })
+
+  // Modified server startup
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  )
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+})()
