e374a3008101a822c51047951db6e11fbfc84b5c
Jonathan Alvarez <j@jonalvarezz.com>
2021-10-10 20:23:25 +0200

    auth: add User model

---
+++ api/prisma/migrations/20211120122810_add_user/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
