968326fcccc621ab8e065faf1bef399b64a959b5
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-06 18:39:12 +0100

    auth: add script to create new admin users

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
-import express from 'express'
 import http from 'http'
 import path from 'path'
 import { PrismaClient } from '@prisma/client'
