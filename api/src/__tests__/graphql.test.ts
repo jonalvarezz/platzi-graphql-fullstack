b796f97bde110273795456ed85814cbfc034ae00
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-08 21:40:51 +0100

    tests: resolver and schema with EasyGraphQL
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/src/__tests__/graphql.test.ts
+++ api/src/__tests__/graphql.test.ts
@@ -1,7 +1,17 @@
+import path from 'path'
+import { readFileSync } from 'fs'
 import { PrismaClient, User, Avocado } from '@prisma/client'
 import { mockDeep } from 'jest-mock-extended'
 import { DeepMockProxy } from 'jest-mock-extended'
-import { ResolverContext, findOne } from '../resolvers/avocado.resolver'
+import gql from 'graphql-tag'
+import EasyGraphQLTester from 'easygraphql-tester'
+
+import { ResolverContext } from '../resolvers/avocado.resolver'
+import resolvers from '../resolvers'
+
+const schema = readFileSync(path.join(__dirname, '../schema.graphql'), 'utf8')
+
+const tester = new EasyGraphQLTester(schema, resolvers)
 
 export type MockResolverContext = {
   orm: DeepMockProxy<PrismaClient>
@@ -35,20 +45,58 @@ const mockAvocadoDB: Avocado[] = [
   },
 ]
 
-describe('findOne', () => {
-  test('should return an Avo with attributes', async () => {
-    mockContext.orm.avocado.findUnique.mockResolvedValue(mockAvocadoDB[0])
+test('should return a list of avos', async () => {
+  mockContext.orm.avocado.findMany.mockResolvedValue(mockAvocadoDB)
 
-    const result = await findOne(undefined, { id: '0' }, context)
-    expect(result).toEqual(mockAvocadoDB[0])
-    expect(mockContext.orm.avocado.findUnique).toHaveBeenCalledTimes(1)
-    expect(mockContext.orm.avocado.findUnique).toHaveBeenCalledWith({
-      where: {
-        id: 0,
+  const query = gql`
+    {
+      avos {
+        id
+        name
+        price
+      }
+    }
+  `
+
+  const result = await tester.graphql(query, undefined, context)
+  expect(mockContext.orm.avocado.findMany).toHaveBeenCalledTimes(1)
+  expect(result.data).toEqual({
+    avos: [
+      {
+        id: '1',
+        name: 'Reed Avocado',
+        price: 1.18,
       },
-      include: {
-        attributes: true,
-      },
-    })
+    ],
+  })
+  expect(mockContext.orm.avocado.findMany).toHaveBeenCalledWith({
+    include: { attributes: true },
+    where: undefined,
+    take: undefined,
+    skip: undefined,
+  })
+})
+
+test('should filter a list of avos', async () => {
+  mockContext.orm.avocado.findMany.mockResolvedValue([])
+
+  const query = gql`
+    {
+      avos(where: { name: { contains: "Hass" } }, skip: 1) {
+        id
+      }
+    }
+  `
+
+  const result = await tester.graphql(query, undefined, context)
+  expect(result.data).toEqual({
+    avos: [],
+  })
+  expect(mockContext.orm.avocado.findMany).toHaveBeenCalledTimes(1)
+  expect(mockContext.orm.avocado.findMany).toHaveBeenCalledWith({
+    include: { attributes: true },
+    where: { name: { contains: 'Hass' } },
+    take: undefined,
+    skip: 1,
   })
 })
