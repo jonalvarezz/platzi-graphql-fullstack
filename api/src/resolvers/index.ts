f3f7f85d3cecafcf6a6d348db40be5959511fc69
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-08 21:40:51 +0100

    tests: resolver and schema with EasyGraphQL
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,8 +1,17 @@
+import type { Avocado } from '@prisma/client'
 import * as avo from './avocado.resolver'
 import * as scalars from './scalars'
 
 export default {
   ...scalars,
+  BaseModel: {
+    __resolveType: (parent: Avocado) => {
+      if (parent.name) {
+        return 'Avocado'
+      }
+      return null // No more implementations
+    },
+  },
   Query: {
     avo: avo.findOne,
     avos: avo.findAll,
