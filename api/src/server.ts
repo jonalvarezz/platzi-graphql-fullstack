cc67950831f83ccb6a3f96d9b73cfeea5f21a7ea
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-17 23:30:21 +0100

    feat: Add a homepage

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,8 +5,14 @@ import session from 'express-session'
 import { urlencoded } from 'body-parser'
 
 import auth, { login, logout, getUserDetail } from './auth'
+import { favicon } from './favicon'
 
 export const app = express()
+
+app.get('/favicon.ico', favicon)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'))
+})
 
 // Middlewares
 app.use('/static', express.static(path.join(__dirname, '../public')))
