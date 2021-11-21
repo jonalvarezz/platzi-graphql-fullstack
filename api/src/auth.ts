5fd816fffe29c297a425f582411a7dbe5e038d0b
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-21 15:26:46 +0100

    prod(auth): support redirects on login/logout
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -1,7 +1,7 @@
 import passport from 'passport'
 import { Strategy as LocalAuth } from 'passport-local'
 import type { VerifyFunction } from 'passport-local'
-import type { Response, Request, NextFunction } from 'express'
+import type { Response, Request, NextFunction, RequestHandler } from 'express'
 import { PrismaClient } from '@prisma/client'
 import { compare } from 'bcrypt'
 
@@ -60,11 +60,51 @@ passport.deserializeUser(async function (userId, done) {
   }
 })
 
-export const login = passport.authenticate('local')
+const getRedirect = (req: Request) => {
+  const redirect = req.body.redirect as string | undefined
 
+  return {
+    redirect,
+    hasRedirect: typeof redirect === 'string',
+  }
+}
+
+/**
+ * Login with redirect support.
+ *
+ * The response will be redirected to the given `redirect` value if any.
+ */
+export const login: RequestHandler = (req, res, next) => {
+  const { redirect, hasRedirect } = getRedirect(req)
+
+  const options = hasRedirect
+    ? {
+        successRedirect: redirect,
+        failureRedirect: `${redirect}/login?_error=${encodeURI(
+          'Wrong credentials'
+        )}`,
+      }
+    : {}
+
+  const auth = passport.authenticate('local', options)
+
+  return auth(req, res, next)
+}
+
+/**
+ * Logout with redirect support.
+ *
+ * The response will be redirected to the given `redirect` value if any.
+ */
 export function logout(req: Request, res: Response, next: NextFunction) {
   req.logout()
+  const { redirect, hasRedirect } = getRedirect(req)
+
+  if (hasRedirect) {
+    res.redirect(redirect!)
+  } else {
   next()
+  }
 }
 
 export default passport
