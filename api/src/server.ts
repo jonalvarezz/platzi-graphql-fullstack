387e06a69f3d445f77b9ebd8a93b519cb9e419f0
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-23 00:45:45 +0100

    auth: enable cors and json parser for login

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
-import { urlencoded } from 'body-parser'
+import cors from 'cors'
+import { urlencoded, json } from 'body-parser'
 
 import auth, { login, currentUser } from './auth'
 import { favicon } from './favicon'
@@ -13,12 +14,13 @@ app.get('/', (req, res) => {
 })
 
 // Middlewares
+app.use(cors())
 app.use('/static', express.static(path.join(__dirname, '../public')))
 app.use(urlencoded({ extended: false }))
 app.use(auth)
 
 // Auth Routes
-app.post('/api/login', login)
+app.post('/api/login', json(), login)
 app.get('/api/user/current', currentUser)
 
 export default app
