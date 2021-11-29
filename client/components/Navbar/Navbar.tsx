4c66d7a440bc97e47b747aa7e6d1f45ce19ac782
Jonathan Alvarez <j@jonalvarezz.com>
2021-11-29 21:03:08 +0100

    👩‍🎨 Add UI from platzi-nextjs

---
+++ client/components/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Menu, Container } from 'semantic-ui-react'
+
+import { Avocado } from '@components/SVGIcons'
+import ShoppingCartIcon from './ShoppingCartIcon'
+import { useCart } from '@store/Cart'
+
+const Navbar = () => {
+  const { pathname } = useRouter()
+  const { count: cartCount } = useCart()
+
+  return (
+    <Menu size="huge" borderless pointing as="header">
+      <Container text>
+        <Link href="/" passHref>
+          <Menu.Item
+            active={pathname === '/'}
+            title="Inicio | Todos los productos"
+          >
+            <Avocado />
+            Avo Store
+          </Menu.Item>
+        </Link>
+        <Menu.Menu position="right">
+          <Link href="/cart" passHref>
+            <Menu.Item active={pathname === '/cart'}>
+              <ShoppingCartIcon cartCount={cartCount} name="Canasta" />
+            </Menu.Item>
+          </Link>
+        </Menu.Menu>
+      </Container>
+      <style jsx global>{`
+        .ui.menu.huge {
+          font-size: 1.5rem;
+        }
+      `}</style>
+    </Menu>
+  )
+}
+
+export default Navbar
