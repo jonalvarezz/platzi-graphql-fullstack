eba765c66dc22bb6859664a5bd78d01040e91dd5
Jonathan Alvarez <j@jonalvarezz.com>
2021-12-05 14:09:42 +0100

    grapqh in nextjs: use static generation for home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,37 @@
-import { useQuery } from '@apollo/client'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import client from '@service/client'
 import { GetAllAvocadosDocument } from '@service/graphql'
 import type { AvocadoFragment } from '@service/graphql'
 
-const HomePage = () => {
-  const { data, loading } = useQuery(GetAllAvocadosDocument)
+export const getStaticProps: GetStaticProps<{ products: AvocadoFragment[] }> =
+  async () => {
+    try {
+      const response = await client.query({
+        query: GetAllAvocadosDocument,
+      })
 
-  let products = (data?.avos || []) as AvocadoFragment[]
+      if (response.data.avos == null) {
+        throw new Error(`There was an error fetching the items`)
+      }
 
+      const products = response.data.avos as AvocadoFragment[]
+      return { props: { products } }
+    } catch (e) {
+      console.log(e)
+      return {
+        props: {
+          products: [],
+        },
+      }
+    }
+  }
+
+const HomePage = ({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout title="Home">
       <KawaiiHeader />
